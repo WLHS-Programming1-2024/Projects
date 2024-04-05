102061ced5735eb467a3da33e62759192d2054f1
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2024-04-05 10:03:09 -0700

    Create geometry_fun.py

---
+++ Project-1-Geometry-Fun/geometry_fun.py
@@ -0,0 +1,37 @@
+# Starter file
+
+'''
+Name:
+Date:
+Assignment:
+Description:
+'''
+
+def square(side_length):
+  """
+  Prints the perimeter and area of a square
+
+  Parameters
+  ----------
+  side_length:int - side length of square
+
+  Return
+  ------
+  None
+  """
+  
+  print("")
+
+def circle(diameter):
+  print("")
+
+def equilateral_triangle(side_length):
+  print("")
+
+
+def main():
+  # call your functions here as needed
+
+# don't change code below this line
+if __name__ == '__main__':
+  main()
